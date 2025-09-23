8b99a616697c613987c6637967b546b427cdab02
Samuel Keller <48340527+twarped@users.noreply.github.com>
2025-09-23 15:10:06 -0600

    feat: updated `loading` state

--- current_state.js
+++ current_state.js
@@ -1,19 +1,19 @@
 (() => {
-    if (document.querySelector('#popup button[aria-label=Dismiss]')) {
+    if (document.querySelector('#popup button[aria-label=Dismiss]'))
         return 'popup';
-    }
-    if (document.getElementById('google-hats-survey')) {
+    if (document.getElementById('google-hats-survey'))
         return 'survey';
-    }
-    if (document.querySelector('#interactive-hovercard')?.style.display == 'block') {
+    if (document.querySelector('#interactive-hovercard')?.style.display == 'block')
         return 'hovercard';
-    }
 
     let feed = document.querySelector('[role=feed]');
     let feedChildren = feed?.children;
     let lastFeedChildChildren = feedChildren?.[feedChildren.length - 1]?.children;
     let feedScrolledToBottom = feed?.scrollHeight - feed?.scrollTop - 1 < feed?.clientHeight;
     let hasFeed = feedChildren && lastFeedChildChildren;
+    // weirdly enough, it seems like if there are less than 4 style elements with a data-late-css
+    // attribute, the page is either loading, or broken. this may change.
+    let styleCount = document.documentElement?.innerHTML.match(/<style\s*(type="[^"]+"|)\s*data-late-css/g)?.length;
     let refreshButton = document.querySelector('[jsaction=\"search.refresh\"]');
     let refreshButtonRect = refreshButton?.getBoundingClientRect();
     let refreshLoadingIcon = refreshButton?.querySelector('span[aria-hidden]');
@@ -21,24 +21,18 @@
     let isEnd = document.querySelector('[role=feed]>div:last-child>div>p') || (hasFeed && !hasResults);
     let feedCount = [...(feed?.querySelectorAll('a.hfpxzc') || [])].length;
     let searchBoxClassCount = document.querySelector('#omnibox-singlebox')?.classList.length;
-    let isLoading = (searchBoxClassCount || (refreshButton && !refreshLoadingIcon)) || (hasResults && feedScrolledToBottom && !isEnd);
+    let isLoading = (searchBoxClassCount || (refreshButton && !refreshLoadingIcon)) || (hasResults && feedScrolledToBottom && !isEnd) || styleCount < 4;
 
-    if (document.querySelector('[role=main]:not(:has([role=feed]))') && hasFeed) {
+    if (document.querySelector('[role=main]:not(:has([role=feed]))') && hasFeed)
         return 'place-open';
-    }
-    if (isLoading) {
+    if (isLoading)
         return 'loading';
-    }
-    if (refreshButtonRect?.top > 0 && refreshButtonRect?.left > 0 && refreshLoadingIcon) {
+    if (refreshButtonRect?.top > 0 && refreshButtonRect?.left > 0 && refreshLoadingIcon)
         return 'refresh';
-    }
-    if (!feedScrolledToBottom && feedChildren && !isEnd) {
+    if (!feedScrolledToBottom && feedChildren && !isEnd)
         return 'scroll';
-    }
-    if (!hasFeed) {
+    if (!hasFeed)
         return 'no-feed';
-    }
-    if (isEnd) {
+    if (isEnd)
         return 'end|' + feedCount;
-    }
 })();
